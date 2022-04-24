eb20831180338c58b3668453f080d6f7e6450b35
saurabh <saurabh.braj@gmail.com>
2022-04-24 19:09:40 +0530

    Add Responsiveness for small screens.

--- dynaminds-charts/src/app/app-routing.module.ts
+++ dynaminds-charts/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: 'pages', loadChildren: () => import('../app/pages/pages.module').then(m => m.PagesModule) }
+  { path: 'pages', loadChildren: () => import('../app/pages/pages.module').then(m => m.PagesModule) },
+  { path: '**', redirectTo: 'pages' }
 ];
 
 @NgModule({
